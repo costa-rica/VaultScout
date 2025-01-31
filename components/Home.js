4f5255561680dbe617ea41996e05ba24a8d7ab58
costa-rica <nrodrig1@gmail.com>
2025-01-31 21:55:55 +0100

    first commit

---
+++ components/Home.js
@@ -0,0 +1,17 @@
+import styles from "../styles/Home.module.css";
+
+function Home() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <div>Kilin: Hacakthon</div>
+        <h2>Vault Scout</h2>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
